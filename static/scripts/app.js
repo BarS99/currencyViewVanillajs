41dd10d760acb09e6bab66d6ce4aa1f7433207f7
Bartlomiej <bartlomiejswiech99@gmail.com>
2021-09-05 16:53:59 +0200

    Add localstorage and clear button
    Co-authored-by: Paweł Piórkowski <InKronos@users.noreply.github.com>

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,7 +1,28 @@
+const generateTable = (currencyViewer) => {
+    let htmlItems = (localStorage.getItem('htmlItems') === undefined || localStorage.getItem('htmlItems') === null) ? 0 : JSON.parse(localStorage.getItem('htmlItems'));
+    console.log(htmlItems);
+    if (htmlItems != 0) {
+        htmlItems.forEach(item => {
+            let row = document.createElement('tr');
+            console.log(item.date);
+            row.innerHTML += `<td>${item.date}</td>`;
+            row.innerHTML += `<td>${item.key}</td>`;
+            row.innerHTML += `<td>${item.rate}</td>`;
+            currencyViewer.appendChild(row);
+        });
+    } else {
+        console.log("No Items");
+    }
+}
+
 window.addEventListener('DOMContentLoaded', () => {
 
     const currencySelect = document.getElementById('currencySelect');
     const currencyViewer = document.getElementById('currencyViewer');
+    const currencyClearButton = document.getElementById('currencyClearButton');
+    //localStorage.removeItem("htmlItems");
+
+    generateTable(currencyViewer);
 
     currencySelect.addEventListener('change', (e) => {
         const currencyFetchResponse = fetch(`http://api.exchangeratesapi.io/v1/latest?access_key=3719758393144ca351490d4c1baef720&symbols=${e.target.value}`)
@@ -23,9 +44,28 @@ window.addEventListener('DOMContentLoaded', () => {
                     row.innerHTML += `<td>${key}</td>`;
                     row.innerHTML += `<td>${data.rates[key]}</td>`;
 
+                    let htmlItems = (localStorage.getItem('htmlItems') === undefined || localStorage.getItem('htmlItems') === null) ? new Array() : JSON.parse(localStorage.getItem('htmlItems'));
+
+                    htmlItems.push({
+                        date: date,
+                        key: key,
+                        rate: data.rates[key]
+                    });
+
                     currencyViewer.appendChild(row);
+                    localStorage.setItem('htmlItems', JSON.stringify(htmlItems));
                 }
             })
             .catch(error => console.log(error));
+    });
+
+    currencyClearButton.addEventListener('click', () => {
+        localStorage.removeItem("htmlItems");
+        let rows = currencyViewer.querySelectorAll('tr:not(:first-child)');
+
+        rows.forEach((item) => {
+            item.remove();
+        })
     })
+
 })
